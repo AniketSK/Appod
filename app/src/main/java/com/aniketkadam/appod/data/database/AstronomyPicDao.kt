c8e0e1fb5070797be0893272c7e69923aeb68cb7
Aniket Kadam <anikadamg@gmail.com>
2019-08-05 20:02:07 +0530

    Set up getting a paged list from the database. The repository is going to handle that.

--- app/src/main/java/com/aniketkadam/appod/data/database/AstronomyPicDao.kt
+++ app/src/main/java/com/aniketkadam/appod/data/database/AstronomyPicDao.kt
@@ -1,6 +1,7 @@
 package com.aniketkadam.appod.data.database
 
 import androidx.lifecycle.LiveData
+import androidx.paging.DataSource
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
@@ -13,4 +14,7 @@ interface AstronomyPicDao {
 
     @Query("SELECT * from AstronomyPic")
     fun getAstronomyPics(): LiveData<List<AstronomyPic>>
+
+    @Query("SELECT * FROM AstronomyPic")
+    fun getAstronomyPicsDataSource(): DataSource.Factory<Int, AstronomyPic>
 }
--- app/src/main/java/com/aniketkadam/appod/mainscreen/data/Repository.kt
+++ app/src/main/java/com/aniketkadam/appod/mainscreen/data/Repository.kt
@@ -1,9 +1,24 @@
 package com.aniketkadam.appod.mainscreen.data
 
+import androidx.lifecycle.LiveData
+import androidx.paging.LivePagedListBuilder
+import androidx.paging.PagedList
+import com.aniketkadam.appod.data.AstronomyPic
+import com.aniketkadam.appod.data.database.AstronomyPicDao
 import javax.inject.Inject
 
-class Repository @Inject constructor() {
-    fun getApodList() {
+private const val PREFETCH_DISTANCE = 50
 
+class Repository @Inject constructor(private val dao: AstronomyPicDao) {
+
+    fun getApodList(): LiveData<PagedList<AstronomyPic>> {
+
+        val localSource = dao.getAstronomyPicsDataSource()
+
+        val config = PagedList.Config.Builder().setPrefetchDistance(PREFETCH_DISTANCE).setPageSize(PREFETCH_DISTANCE)
+            .setMaxSize(PREFETCH_DISTANCE * 2)
+            .build()
+
+        return LivePagedListBuilder(localSource, config).build()
     }
 }
