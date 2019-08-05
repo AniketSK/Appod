c1df50e35716584ddf82b6023552a3a82a213ae4
Aniket Kadam <anikadamg@gmail.com>
2019-08-05 20:14:37 +0530

    Added jodatime because we'll need it to calculate the date ranges for the apod call. Updated its version in the dependencies.

--- app/src/androidTest/java/com/aniketkadam/appod/DaggerStubApplication.kt
+++ app/src/androidTest/java/com/aniketkadam/appod/DaggerStubApplication.kt
@@ -1,7 +1,12 @@
 package com.aniketkadam.appod
 
 import android.app.Application
+import net.danlew.android.joda.JodaTimeAndroid
 
 class DaggerStubApplication : Application() {
 
+    override fun onCreate() {
+        super.onCreate()
+        JodaTimeAndroid.init(this)
+    }
 }
--- app/src/main/java/com/aniketkadam/appod/AppodApplication.kt
+++ app/src/main/java/com/aniketkadam/appod/AppodApplication.kt
@@ -3,12 +3,18 @@ package com.aniketkadam.appod
 import com.aniketkadam.appod.di.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
+import net.danlew.android.joda.JodaTimeAndroid
 
 
 class AppodApplication : DaggerApplication() {
 
     private val appInjector by lazy {
         DaggerAppComponent.builder().application(this).build()
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        JodaTimeAndroid.init(this)
     }
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> = appInjector
