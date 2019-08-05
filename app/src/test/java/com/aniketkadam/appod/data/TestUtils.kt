b61d9e03e4bb23f5efa47fe8cb027a73a86281df
Aniket Kadam <anikadamg@gmail.com>
2019-08-05 18:27:04 +0530

    Added utilities for testing

--- app/src/test/java/com/aniketkadam/appod/data/TestUtils.kt
+++ app/src/test/java/com/aniketkadam/appod/data/TestUtils.kt
@@ -1,4 +1,27 @@
 package com.aniketkadam.appod.data
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
+
 fun getTextInFile(fileName: String): String? =
     ClassLoader.getSystemResourceAsStream(fileName)?.bufferedReader().use { it?.readText() }
+
+@Throws(InterruptedException::class)
+fun <T> getValueForLiveData(liveData: LiveData<T>): T {
+    val data = arrayOfNulls<Any>(1)
+    val latch = CountDownLatch(1)
+    val observer = object : Observer<T> {
+        override fun onChanged(o: T?) {
+            data[0] = o
+            latch.countDown()
+            liveData.removeObserver(this)
+        }
+    }
+    liveData.observeForever(observer)
+    latch.await(2, TimeUnit.SECONDS)
+
+    @Suppress("UNCHECKED_CAST")
+    return data[0] as T
+}
