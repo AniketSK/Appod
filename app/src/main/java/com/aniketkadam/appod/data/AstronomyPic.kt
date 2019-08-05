b830022b528e4c54ec8e3538d033b5c711a2945d
Aniket Kadam <anikadamg@gmail.com>
2019-08-05 15:54:13 +0530

    Creating a data class for the relevant sections of the pic data that we'll need

---
+++ app/src/main/java/com/aniketkadam/appod/data/AstronomyPic.kt
@@ -0,0 +1,9 @@
+package com.aniketkadam.appod.data
+
+data class AstronomyPic(
+    val title: String,
+    val url: String,
+    val hdurl: String,
+    val explanation: String,
+    val date: String
+)
