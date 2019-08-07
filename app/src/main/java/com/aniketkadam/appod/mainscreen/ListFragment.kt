c734aef4d15681c809c2fef0a0753ecf3286a08c
Aniket Kadam <anikadamg@gmail.com>
2019-08-07 08:00:29 +0530

    Using a GridLayoutManager that's set to span 3 columns to show the images. This gets them actually in a visual order that makes sense.

--- app/src/main/java/com/aniketkadam/appod/mainscreen/ListFragment.kt
+++ app/src/main/java/com/aniketkadam/appod/mainscreen/ListFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.GridLayoutManager
 import com.aniketkadam.appod.R
 import com.aniketkadam.appod.mainscreen.di.MAIN_FRAGMENT_VM
 import com.aniketkadam.appod.mainscreen.vm.MainVm
@@ -26,6 +27,7 @@ class ListFragment : DaggerFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val adapter = PagedAdapter()
+        gridRecyclerView.layoutManager = GridLayoutManager(context, 3)
         gridRecyclerView.adapter = adapter
 
         mainVm.apodList.observe(this, Observer { adapter.submitList(it) })
