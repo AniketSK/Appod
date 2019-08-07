5656c9074f0ff6ab79ddeacea7401a06279db484
Aniket Kadam <anikadamg@gmail.com>
2019-08-07 14:49:02 +0530

    Checking that the networkstate is actually set to success after the onZeroItemsLoaded call goes through

--- app/src/test/java/com/aniketkadam/appod/mainscreen/data/ApodNetworkBoundaryCallbackTest.kt
+++ app/src/test/java/com/aniketkadam/appod/mainscreen/data/ApodNetworkBoundaryCallbackTest.kt
@@ -1,14 +1,20 @@
 package com.aniketkadam.appod.mainscreen.data
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.aniketkadam.appod.RxImmediateSchedulerRule
 import com.aniketkadam.appod.data.ApodApi
 import com.aniketkadam.appod.data.AstronomyPic
 import com.aniketkadam.appod.data.database.AstronomyPicDao
+import com.aniketkadam.appod.data.getValueForLiveData
 import io.mockk.MockKAnnotations
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.verify
 import io.reactivex.Single
+import org.hamcrest.Matchers.equalTo
+import org.junit.Assert.assertThat
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 
 
@@ -23,6 +29,11 @@ class ApodNetworkBoundaryCallbackTest {
     lateinit var b: ApodNetworkBoundaryCallback
     private val pic = AstronomyPic("", "", "", "", "")
 
+    @get:Rule
+    val immediateSchedulerRule = RxImmediateSchedulerRule()
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
     @Before
     fun setUp() {
         MockKAnnotations.init(this)
@@ -36,6 +47,14 @@ class ApodNetworkBoundaryCallbackTest {
         b.onZeroItemsLoaded()
         verify(exactly = 1) { api.getApodList(any()) }
         verify(exactly = 1) { dao.insert(any()) }
+    }
+
+    @Test
+    fun `the network state is set to success after a onZeroItemsLoaded call is complete`() {
+        every { dao.insert(any()) } returns Unit
+        b = ApodNetworkBoundaryCallback(api, dao, requestDates)
+        b.onZeroItemsLoaded()
+        assertThat(getValueForLiveData(b.networkCallState), equalTo<ApodCallState>(ApodCallState.Success))
     }
 
 }
