a6d08c72f78d78ad9f201017a2c07803b858316b
Aniket Kadam <anikadamg@gmail.com>
2019-08-07 07:03:04 +0530

    Fixed the appcomponentfortest using the regular android injection instead of support injection which it should've been using.

--- app/src/androidTest/java/com/aniketkadam/appod/di/AppComponentTest.kt
+++ app/src/androidTest/java/com/aniketkadam/appod/di/AppComponentTest.kt
@@ -4,15 +4,14 @@ import android.app.Application
 import com.aniketkadam.appod.DaggerStubApplication
 import dagger.BindsInstance
 import dagger.Component
-import dagger.android.AndroidInjectionModule
 import dagger.android.AndroidInjector
+import dagger.android.support.AndroidSupportInjectionModule
 import javax.inject.Singleton
 
 @Singleton
 @Component(
-    modules = [AndroidInjectionModule::class,
+    modules = [AndroidSupportInjectionModule::class,
         DatabaseModuleForTest::class,
-        NetworkingModule::class,
         ActivityBuilderModuleForTest::class,
         AppModule::class]
 )
