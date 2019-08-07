f7f9db2ab09e69af86652fe6d83db39835d35c41
Aniket Kadam <anikadamg@gmail.com>
2019-08-07 07:52:32 +0530

    Making the readAssets extension function public. Will be used shortly.

--- app/src/androidTest/java/com/aniketkadam/appod/ExtensionFunctions.kt
+++ app/src/androidTest/java/com/aniketkadam/appod/ExtensionFunctions.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 
-private inline fun <reified T : Any> Context.readAssetsFile(fileName: String): T =
+inline fun <reified T : Any> Context.readAssetsFile(fileName: String): T =
     assets.open(fileName).bufferedReader().use { it.readText() }.let {
         Gson().fromJson(it, object : TypeToken<T>() {}.type)
     }
