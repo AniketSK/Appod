7433e10c93ffec5428dbf54d3252d94259ede149
Aniket Kadam <anikadamg@gmail.com>
2019-08-07 14:30:01 +0530

    Modified one of the tests to use mockk instead of mock. Was tracking down a different issue and happened to do this.

--- app/src/test/java/com/aniketkadam/appod/mainscreen/data/ApodNetworkBoundaryCallbackTest.kt
+++ app/src/test/java/com/aniketkadam/appod/mainscreen/data/ApodNetworkBoundaryCallbackTest.kt
@@ -3,30 +3,30 @@ package com.aniketkadam.appod.mainscreen.data
 import com.aniketkadam.appod.data.ApodApi
 import com.aniketkadam.appod.data.AstronomyPic
 import com.aniketkadam.appod.data.database.AstronomyPicDao
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.verify
 import io.reactivex.Single
 import org.junit.Before
 import org.junit.Test
-import org.mockito.Mock
-import org.mockito.Mockito.times
-import org.mockito.Mockito.verify
-import org.mockito.MockitoAnnotations
-import org.mockito.Mockito.`when` as When
+
 
 class ApodNetworkBoundaryCallbackTest {
 
-    @Mock
+    @MockK
     lateinit var api: ApodApi
-    @Mock
+    @MockK
     lateinit var dao: AstronomyPicDao
-    @Mock
+    @MockK
     lateinit var requestDates: ApodRequestDates
     lateinit var b: ApodNetworkBoundaryCallback
     private val pic = AstronomyPic("", "", "", "", "")
 
     @Before
     fun setUp() {
-        MockitoAnnotations.initMocks(this)
-        When(api.getApodList(requestDates)).thenReturn(Single.just(listOf(pic)))
+        MockKAnnotations.init(this)
+        every { api.getApodList(any()) } answers { Single.just(listOf(pic)) }
     }
 
     @Test
@@ -34,8 +34,8 @@ class ApodNetworkBoundaryCallbackTest {
         b = ApodNetworkBoundaryCallback(api, dao, requestDates)
         b.onZeroItemsLoaded()
         b.onZeroItemsLoaded()
-        verify(api, times(1)).getApodList(requestDates)
-        verify(dao, times(1)).insert(listOf(pic))
+        verify(exactly = 1) { api.getApodList(any()) }
+        verify(exactly = 1) { dao.insert(any()) }
     }
 
 }
