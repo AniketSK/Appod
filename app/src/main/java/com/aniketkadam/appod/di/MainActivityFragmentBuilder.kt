d18bdf20be4e820f5688e14e83a9ecd546bc6ffb
Aniket Kadam <anikadamg@gmail.com>
2019-08-07 11:31:30 +0530

    Added DetailFragment to the fragment builder for injection.

--- app/src/main/java/com/aniketkadam/appod/di/MainActivityFragmentBuilder.kt
+++ app/src/main/java/com/aniketkadam/appod/di/MainActivityFragmentBuilder.kt
@@ -1,5 +1,6 @@
 package com.aniketkadam.appod.di
 
+import com.aniketkadam.appod.mainscreen.DetailFragment
 import com.aniketkadam.appod.mainscreen.ListFragment
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
@@ -8,4 +9,7 @@ import dagger.android.ContributesAndroidInjector
 abstract class MainActivityFragmentBuilder {
     @ContributesAndroidInjector
     abstract fun bindListFragment(): ListFragment
+
+    @ContributesAndroidInjector
+    abstract fun bindDetailFragment(): DetailFragment
 }
