088acd1cb5e7434c31975461ba6287072783eea4
Aniket Kadam <anikadamg@gmail.com>
2019-08-08 15:26:15 +0530

    Added a toast for when there's an error during refresh.

--- app/src/main/java/com/aniketkadam/appod/mainscreen/apodlist/ListFragment.kt
+++ app/src/main/java/com/aniketkadam/appod/mainscreen/apodlist/ListFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.GridLayoutManager
@@ -73,7 +74,10 @@ class ListFragment : DaggerFragment() {
     private fun renderSwipeRefresh(state: RefreshLce?): Unit = when (state) {
         is RefreshLce.Loading -> swipeRefreshView.isRefreshing = true
         is RefreshLce.Success -> swipeRefreshView.isRefreshing = false
-        is RefreshLce.Error -> swipeRefreshView.isRefreshing = false
+        is RefreshLce.Error -> {
+            swipeRefreshView.isRefreshing = false
+            Toast.makeText(this.context, R.string.internet_error, Toast.LENGTH_LONG).show()
+        }
         null -> Timber.d("Got an empty on the swipe refreshEffects view")
     }
 
