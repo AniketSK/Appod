0c0fc94ddeace88883fe6b14f1f2eb123947d729
Aniket Kadam <anikadamg@gmail.com>
2019-08-08 16:43:18 +0530

    Fixed the layout for the detail view by separating it from what's needed in the main list.

--- app/src/main/java/com/aniketkadam/appod/mainscreen/apoddetail/DetailFragment.kt
+++ app/src/main/java/com/aniketkadam/appod/mainscreen/apoddetail/DetailFragment.kt
@@ -29,7 +29,7 @@ class DetailFragment : DaggerFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         super.onCreateView(inflater, container, savedInstanceState)
-        return inflater.inflate(R.layout.fragment_layout, container, false)
+        return inflater.inflate(R.layout.detail_fragment_layout, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
