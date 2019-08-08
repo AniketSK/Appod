ab7b206187becc04de7f02d67ba737e13197fc9c
Aniket Kadam <anikadamg@gmail.com>
2019-08-08 15:44:52 +0530

    Prevent crashes from the navcontroller trying to navigate to the same fragment when it's on that fragment. Just always use the methods to construct directions and pass them to the saveNavigate extension function instead.

--- app/src/main/java/com/aniketkadam/appod/mainscreen/MainActivity.kt
+++ app/src/main/java/com/aniketkadam/appod/mainscreen/MainActivity.kt
@@ -2,6 +2,8 @@ package com.aniketkadam.appod.mainscreen
 
 import android.os.Bundle
 import androidx.lifecycle.Observer
+import androidx.navigation.NavController
+import androidx.navigation.NavDirections
 import androidx.navigation.findNavController
 import com.aniketkadam.appod.R
 import com.aniketkadam.appod.mainscreen.apoddetail.DetailFragmentDirections
@@ -11,6 +13,7 @@ import com.aniketkadam.appod.mainscreen.vm.ActiveFragmentPosition
 import com.aniketkadam.appod.mainscreen.vm.MainVm
 import com.aniketkadam.appod.mainscreen.vm.PositionFragment
 import dagger.android.support.DaggerAppCompatActivity
+import timber.log.Timber
 import javax.inject.Inject
 import javax.inject.Named
 
@@ -29,12 +32,12 @@ class MainActivity : DaggerAppCompatActivity() {
 
     private fun setActive(positionFragment: PositionFragment?) = with(findNavController(R.id.nav_host)) {
         when (positionFragment?.fragment) {
-            ActiveFragmentPosition.LIST_FRAGMENT -> navigate(
+            ActiveFragmentPosition.LIST_FRAGMENT -> safeNavigate(
                 DetailFragmentDirections.actionDetailFragmentToListFragment().setAdapterPosition(
                     positionFragment.position
                 )
             )
-            ActiveFragmentPosition.DETAIL_FRAGMENT -> navigate(
+            ActiveFragmentPosition.DETAIL_FRAGMENT -> safeNavigate(
                 ListFragmentDirections.actionListFragmentToDetailFragment().setAdapterPosition(
                     positionFragment.position
                 )
@@ -44,3 +47,7 @@ class MainActivity : DaggerAppCompatActivity() {
     }
 
 }
+
+private fun NavController.safeNavigate(d: NavDirections) =
+    currentDestination?.getAction(d.actionId)?.let { navigate(d) }
+        ?: Timber.e("Invalid route for direction ${d} with id ${d.actionId}")
