cc489bcba46ce6fad9d12d405660b583e42bbe12
Aniket Kadam <anikadamg@gmail.com>
2019-08-10 12:28:07 +0000

    PagedAdapterDiffs was reversed

--- app/src/main/java/com/aniketkadam/appod/mainscreen/PagedAdapterDiffs.kt
+++ app/src/main/java/com/aniketkadam/appod/mainscreen/PagedAdapterDiffs.kt
@@ -6,10 +6,10 @@ import com.aniketkadam.appod.data.AstronomyPic
 class PagedAdapterDiffs {
     companion object {
         val DIFF_CALLBACK = object : DiffUtil.ItemCallback<AstronomyPic>() {
-            override fun areItemsTheSame(oldItem: AstronomyPic, newItem: AstronomyPic): Boolean = oldItem == newItem
+            override fun areItemsTheSame(oldItem: AstronomyPic, newItem: AstronomyPic): Boolean = oldItem.date == newItem.date
 
             override fun areContentsTheSame(oldItem: AstronomyPic, newItem: AstronomyPic): Boolean =
-                oldItem.date == newItem.date
+                oldItem == newItem
 
         }
     }
