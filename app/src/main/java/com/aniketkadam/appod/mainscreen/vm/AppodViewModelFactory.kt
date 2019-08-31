a0e15af645870c7b6605a7a7a058f20628192cca
Aniket Kadam <anikadamg@gmail.com>
2019-08-31 18:29:04 +0530

    Added better errors when a viewmodel wasn't registered with the factory.

--- app/src/main/java/com/aniketkadam/appod/mainscreen/vm/AppodViewModelFactory.kt
+++ app/src/main/java/com/aniketkadam/appod/mainscreen/vm/AppodViewModelFactory.kt
@@ -3,15 +3,14 @@ package com.aniketkadam.appod.mainscreen.vm
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.aniketkadam.appod.mainscreen.data.Repository
-import javax.inject.Inject
 
-class AppodViewModelFactory @Inject constructor(private val repository: Repository) : ViewModelProvider.Factory {
+class AppodViewModelFactory(private val repository: Repository) : ViewModelProvider.Factory {
 
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel?> create(modelClass: Class<T>): T = with(modelClass) {
         when {
             isAssignableFrom(MainVm::class.java) -> MainVm(repository)
-            else -> throw IllegalArgumentException("what")
+            else -> throw IllegalArgumentException("${modelClass.name} is not a known ViewModel to ${AppodViewModelFactory::class.java.name}")
         }
     } as T
 }
